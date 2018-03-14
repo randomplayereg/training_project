734aa56b5a3d54a4de75fdceb10902936e3b1e5a
randomplayereg <randomplayer.eg@gmail.com>
2018-03-14 15:47:41 +0700

    Log in/out sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,9 +30,12 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
+        session[:user_id] = @user.id
+        redirect_to root_url, notice: "Thank you for signing up!"
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+        render "new"
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :sessions
+
   resources :users
   get 'welcome/index'
 
